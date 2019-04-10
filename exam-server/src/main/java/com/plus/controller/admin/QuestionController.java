fd019aab5cb1d161697b452b573c9e11b49ac8f4
Gyx'mac <gyx@mac.com>
2019-04-10 23:53:47 +0800

    start write question query

---
+++ exam-server/src/main/java/com/plus/controller/admin/QuestionController.java
@@ -0,0 +1,13 @@
+package com.plus.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/question")
+public class QuestionController {
+
+    @GetMapping(value = "query")
+    public
+}
